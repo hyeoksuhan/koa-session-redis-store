c4cea6a5e43ffea8b9fb51405a6910052ed3dda9
hshan <hyeoksu.han@gmail.com>
2018-02-28 10:04:28 +0900

    Modularization

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const RedisStore = require('./lib/redis_store.js');
-const redisStore = new RedisStore();
-redisStore.on('ready', function() {
-  console.log('redis ready');
-
-  this.set("a", {a:10}, 100000, {rolling: true, changed: true})
-  .then(function(res) {
-    console.log(res);
-  })
-});
+module.exports = require('./lib/redis_store.js');
